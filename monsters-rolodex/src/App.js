021949bd05754446249d4032ddc5ac4da886987d
salimmanji <salim@drifttech.io>
2022-09-01 18:33:00 -0600

    reflow

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 
 const App = () => {
   const [searchField, setSearchField] = useState(''); // [value, setValue]
+  const [title, setTitle] = useState('');
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
@@ -20,7 +21,7 @@ const App = () => {
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => setMonsters(users));
+      .then((users) => setMonsters(users))
   }, []);
 
 
@@ -29,10 +30,17 @@ const App = () => {
     setSearchField(searchString);
   };
 
+  const onTitleChange = (event) => {
+    const searchString = event.target.value.toLowerCase();
+    setTitle(searchString);
+  };
+
   return (
     <div className="App">
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>{title}</h1>
       <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='monsters-search-box' />
+      <br />
+      <SearchBox onChangeHandler={onTitleChange} placeholder='set title' className='title-search-box' />
       <CardList monsters={filteredMonsters} />
     </div>
   );
