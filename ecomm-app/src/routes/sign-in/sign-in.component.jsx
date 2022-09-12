569825f7de103fcf84d8643235a0e3a3df8d7fbc
salimmanji <salim@drifttech.io>
2022-09-12 17:05:05 -0600

    google auth redirect

--- ecomm-app/src/routes/sign-in/sign-in.component.jsx
+++ ecomm-app/src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,26 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { async } from '@firebase/util';
 
 const SignIn = () => {
+    // useEffect(() => {
+    //     async function doStuff() {
+    //         const response = await getRedirectResult(auth);
+    //         console.log(response);
+    //         if (response) {
+    //             const userDocRef = await createUserDocumentFromAuth(response.user);
+    //         }
+    //     }
+    //     doStuff();
+    // }, [])
+
     const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
+        const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-    }
+    };
+
 
     return (
         <div>
@@ -12,6 +28,9 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
             </button>
+            {/* <button onClick={signInWithGoogleRedirect}>
+                Sign in with Google Redirect
+            </button> */}
         </div>
     );
 };
